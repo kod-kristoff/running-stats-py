4255ab89ad8df8fb9adbf1f2e790a590d3ee34fe
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-10-14 09:53:06 +0200

    refactor: format changes

--- running_stats/running_stats.py
+++ running_stats/running_stats.py
@@ -7,8 +7,8 @@ import attr
 
 @attr.s(auto_attribs=True)
 class RunningMeanVar:
-    """Compute mean and variance.
-    """
+    """Compute mean and variance."""
+
     n: int = 0
     M1: float = 0.0
     M2: float = 0.0
@@ -17,7 +17,7 @@ class RunningMeanVar:
         """Add a value."""
         self.n += 1
         delta = value - self.M1
-        self.M1 += delta/self.n
+        self.M1 += delta / self.n
         self.M2 += delta * (value - self.M1)
 
     def push_iter(self, values: typing.Iterable[float]):
@@ -31,7 +31,7 @@ class RunningMeanVar:
 
     def variance(self) -> float:
         """Compute the sample variance."""
-        return self.M2/(self.n - 1) if self.n > 1 else 0.0
+        return self.M2 / (self.n - 1) if self.n > 1 else 0.0
 
     def standard_deviation(self) -> float:
         """Compute the sample standard deviation."""
@@ -44,8 +44,7 @@ class RunningMeanVar:
         delta = other.M1 - self.M1
         delta2 = delta * delta
 
-        combined.M1 = (self.M1 * self.n + other.M1 * other.n)/combined.n
+        combined.M1 = (self.M1 * self.n + other.M1 * other.n) / combined.n
         combined.M2 = self.M2 + other.M2 + delta2 * self.n * other.n / combined.n
 
         return combined
-
